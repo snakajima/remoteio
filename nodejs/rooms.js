e57b671d1b081941e5a6c46353038ae3a402432e
Satoshi Nakajima <satoshi@uievolution.com>
2016-11-15 09:59:40 -0800

    join

--- nodejs/rooms.js
+++ nodejs/rooms.js
@@ -1,5 +1,25 @@
 module.exports = function(io) {
+  var _self = this;
+  var rooms = {};
+  
+  console.log(rooms);
+  
+  function Room(_name) {
+    this.name = _name;
+  }
+  
   io.on('connection', function(socket) {
     console.log('connection');
+    
+    socket.on('/room/join', function(data) {
+      console.log('join', rooms, data.name, rooms[data.name]);
+      if (rooms[data.name] == undefined) {
+        rooms[data.name] = new Room(data.name);
+        console.log('room created', rooms);
+      }
+      socket.join(data.name);
+      io.sockets.adapter.sids[socket.id].ROOM = data.name;
+      socket.emit('/room/join/success');
+    });
   });
 }
